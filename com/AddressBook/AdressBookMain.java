912ad4dbec01f22085b6f957c8e740a734718270
Azeempasha471 <abdulazeem1023@gmail.com>
2021-06-29 12:49:11 +0530

    create addressbook with contact details

--- com/AddressBook/AdressBookMain.java
+++ com/AddressBook/AdressBookMain.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 
 public class AdressBookMain {
 	static ArrayList<ContactInfo> user = new ArrayList<ContactInfo>();
-    //static int flag = 0;
 
-    public static void addContact() {
-        ContactInfo a1 = new ContactInfo();
-        a1.getInput();
-        user.add(a1);
-        System.out.println(user);
-    }
+	public static void addContact() {
+		ContactInfo a1 = new ContactInfo();
+		a1.getInput();
+		user.add(a1);
+		System.out.println(user);
+	}
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Programme");
-        //UC1
-        AdressBookMain.addContact();
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Programme");
+		// UC1
+		AdressBookMain.addContact();
 
-    }
+	}
 }
