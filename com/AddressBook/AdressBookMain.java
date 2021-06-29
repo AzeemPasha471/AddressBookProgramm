c7b649427523758122f4702cb03b4193721627c9
Azeempasha471 <abdulazeem1023@gmail.com>
2021-06-29 12:45:32 +0530

    add contact in address book

--- com/AddressBook/AdressBookMain.java
+++ com/AddressBook/AdressBookMain.java
@@ -1,22 +1,34 @@
 package com.AddressBook;
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AdressBookMain {
-	static ArrayList<ContactInfo> user = new ArrayList<ContactInfo>();
-    //static int flag = 0;
+	
+	 static ArrayList<ContactInfo> user = new ArrayList<ContactInfo>();
 
-    public static void addContact() {
-        ContactInfo a1 = new ContactInfo();
-        a1.getInput();
-        user.add(a1);
-        System.out.println(user);
-    }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Programme");
-        //UC1
-        AdressBookMain.addContact();
+	    public static void addContact() {
+	        ContactInfo a1 = new ContactInfo();
+	        a1.getInput();
+	        user.add(a1);
+	        System.out.println(user);
+	    }
 
-    }
-}
+	    public static void addnewContact() {
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("add contact");
+	        AdressBookMain.addContact();
+	    }
+	               
+	           
+
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book Programme");
+	        //UC1
+	        AdressBookMain.addContact();
+	        //UC2
+	        AdressBookMain.addnewContact();
+
+
+	    }
+	}
